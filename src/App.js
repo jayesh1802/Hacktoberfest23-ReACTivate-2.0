2b76d6c383892cc2ceaacaae18c8f1238740e05c
Abhishek Abbi <abhishekabbi.work@gmail.com>
2023-10-06 15:10:59 +0530

    Added list of Clubs and committees

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Clublist from './Clublist';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div>
+//       <div class="topnav">
+//         <a class="active" href="#home">Home</a>
+//         <a href="#about">About</a>
+//         <a href="#contact">Contact</a>
+//         <input type="text" placeholder="Search.."/>
+//     </div>
+//     </div>
+   
+//   );
+// }
 
-export default App;
+export default Clublist;
