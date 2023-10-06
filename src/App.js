41937c162587ff33c449d317fa99819214db1c0b
Abhishek Abbi <abhishekabbi.work@gmail.com>
2023-10-06 15:25:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,6 @@
 import './App.css';
 import Clublist from './Clublist';
 
-// function App() {
-//   return (
-//     <div>
-//       <div class="topnav">
-//         <a class="active" href="#home">Home</a>
-//         <a href="#about">About</a>
-//         <a href="#contact">Contact</a>
-//         <input type="text" placeholder="Search.."/>
-//     </div>
-//     </div>
-   
-//   );
-// }
+
 
 export default Clublist;
