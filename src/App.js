839806f0c403bcdc10106362ecd70ac614f560dd
Jayesh <jayeshdpadiya1802@gmail.com>
2023-10-28 19:11:59 +0530

    DCEI

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import Clublist from './Clublist';
 import Academic from './committees/Academic_Committee';
 
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-
-
 import React from 'react'
 import Dcei from './committees/DCEI';
+import Sambhav from './committees/Sambhav';
 
 const App = () => {
   return (
@@ -17,6 +16,7 @@ const App = () => {
             <Route path='/' element={<Clublist/>}></Route>
             <Route path='/1' element={<Academic/>}></Route>
             <Route path='/14' element={<Dcei/>}></Route>
+            <Route path ='/19' element={<Sambhav/>}></Route>
         </Routes>
         </BrowserRouter>
     </div>
