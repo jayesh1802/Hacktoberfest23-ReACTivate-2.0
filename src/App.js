7afe5fe6e5a1ec0fa65f3fe9cff7d4c5a30e42ed
Jayesh <jayeshdpadiya1802@gmail.com>
2023-10-29 22:35:38 +0530

    added Sambhav Page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import React from 'react'
 import Dcei from './committees/DCEI';
 import Sambhav from './committees/Sambhav';
 import AI_club from './committees/AI_Club';
+import FilmClub from './committees/Film_Club';
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
             <Route path='/14' element={<Dcei/>}></Route>
             <Route path ='/19' element={<Sambhav/>}></Route>
             <Route path='/2' element={<AI_club/>}></Route>
+            {/* <Route path='/21' element={<FilmClub/>}></Route> */}
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/committees/FilmClub Components/FilmEvents.js
@@ -0,0 +1,26 @@
+// import FilmCountdown from "./FilmCountdown";
+
+// const FilmEvents = ({ image,eventdate,month,year,eventName,description,date }) => {
+//     return (
+
+//         <div>
+//             <div className="film-event-container">
+//                 <div className="film-event-box">
+//                     <div className="film-date">
+//                         <p>{eventdate}</p>
+//                         <p>{month} {year}</p>
+//                     </div>
+//                     <div>
+//                         <h2>{eventName}</h2>
+//                         <p>{description}</p>
+//                         <FilmCountdown targetDate={date}/>
+//                     </div>                    
+//                     <img src={image} className="film-image"></img>
+//                 </div>
+
+//             </div>
+
+//         </div>
+//     )
+// }
+// export default FilmEvents;
